e2dbd95c325906f95d2d3d89687b30de89ffad8a
Kemar Porter <porterkemar@ymail.com>
2018-01-09 19:09:38 -0500

    three files added for dom mini challenge

---
+++ main.js
@@ -0,0 +1,19 @@
+function speak () {
+
+ // document.write("im right");
+ var t = document.getElementById("f");
+	t.innerText ="I am right";
+
+
+}
+
+function speak2() {
+	// body...
+	var s = document.getElementById("f");
+	s.innerText ="No I am right";
+}
+
+function alertMe(){
+	alert("Hey I told you not to hover over me!");
+	
+}
