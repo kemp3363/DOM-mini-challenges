7b2d6d9bc99d87fae1ebeb72928bc6993801cca5
Kemar Porter <porterkemar@ymail.com>
2018-01-09 19:17:20 -0500

    added comments

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
+//Adds text to button named first
 function speak () {
-
- // document.write("im right");
  var t = document.getElementById("f");
 	t.innerText ="I am right";
-
-
 }
 
+
+//Changes text next to button named second
 function speak2() {
 	// body...
 	var s = document.getElementById("f");
 	s.innerText ="No I am right";
 }
 
+//Changes text when mouse hovers over text
 function alertMe(){
 	alert("Hey I told you not to hover over me!");
 	
